a086c7bdce7b6ad27573c4c8d5a59d5b66f9a90a
Steven Vandegrift <svandegrift@icloud.com>
2024-01-14 18:48:43 -0800

    Add ability to copy to clipboard

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -20,26 +20,35 @@ def generate_password():
     else: digit_len = pow(10,(int(length) - password_length))
     random_numbers = random.randint(digit_len,digit_len*9)
     generated_password = f"{rand_interest}{rand_symbol}{random_numbers}"
-    new_password.config(text=generated_password)
+    new_password.config(text=generated_password, borderwidth=5, relief="flat", bg='grey')
     
-    
+def copy_to_clipboard(event):
+    root.clipboard_clear()
+    root.clipboard_append(new_password.cget("text"))
+    copy_lbl.grid(row=6, column=0, columnspan=2)
+    root.after(1000, copy_lbl.grid_forget)
 
 root = Tk()
 root.title('Password Generator')
 root.geometry("400x400")
 root.resizable(False, False)
 
+
 center_frame = Frame(root)
 center_frame.pack(expand=True, side='top', anchor='n')
+scrollbar = Scrollbar(center_frame)
 
-# Widgets
+# Frame Widgets
 title_label = Label(center_frame, text="Password Generator", font=("Helvetica", 16))
 len_entry = Entry(center_frame)
 len_label = Label(center_frame, text="Enter Password Length", font=("Helvetica", 12), justify='right')
 symbol_lbl = Label(center_frame, text="Enter Symbols", font=("Helvetica", 12), justify='right')
 symbol_entry = Entry(center_frame)
-enter_button = Button(center_frame, text="Generate", command=generate_password)
-new_password = Label(center_frame, text="", font=("Helvetica", 20), justify='center')
+generate_button = Button(center_frame, text="Generate", command=generate_password)
+new_password = Label(center_frame, text="", font=("Courier New", 12), justify='center', wraplength=root.winfo_screenwidth())
+new_password.bind("<Button-1>", copy_to_clipboard)
+copy_lbl = Label(center_frame, text="Copied to Clipboard!", font=("Helvetica", 8), justify='center', fg='lightgrey')
+
 
 # Grid Layout
 title_label.grid(row=0, column=0, columnspan=2, pady=10)  # spans two columns
@@ -47,8 +56,8 @@ len_label.grid(row=1, column=0, pady=10)  # placed at row 1, column 0
 len_entry.grid(row=1, column=1)  # placed at row 1, column 1
 symbol_lbl.grid(row=2, column=0, pady=10)  # placed at row 1, column 0
 symbol_entry.grid(row=2, column=1)  # placed at row 1, column 1
-enter_button.grid(row=4, column=0, columnspan=2, pady=20)  # placed at row 1, column 1)
-new_password.grid(row=5, column=0, columnspan=2, pady=20)
+generate_button.grid(row=4, column=0, columnspan=2, pady=20)  # placed at row 1, column 1)
+new_password.grid(row=5, column=0, columnspan=2)  
 
 
 
